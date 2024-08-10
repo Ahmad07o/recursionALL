d44ca946b271eb426bbf18e6500238b2bd00a36d
Ahmad07o <aj120262@gmail.com>
2024-08-10 21:34:20 +0500

    hand shake

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         // System.out.println(factorial(7));
-        String s="racecar";
-        int length=s.length();
+        String s = "racecar";
+        int length = s.length();
         /* if(isPalindrome(s,length)==1)
             System.out.println(s + " is palindrom");
          else
             System.out.println(s + " is not a palindrome");
 
-         */
-        System.out.println(countDigit(220,2));
+
+        System.out.println(countDigit(220, 2));*/
+        System.out.println(handShake(5));
     }
-    public static int factorial(int num)
-    {
-        if(num==1)
+
+    public static int factorial(int num) {
+        if (num == 1)
             return 1;
         else
-            return num*factorial(num-1);
+            return num * factorial(num - 1);
     }
+
     public static int isPalindrome(String s, int len) {
         if (len <= 1) {
             return 1; // A string of length 1 or empty string is a palindrome
@@ -27,18 +29,33 @@ public class Main {
             return isPalindrome(s.substring(1, len - 1), len - 2); // Check the remaining substring
         }
     }
-    public static int countDigit(int n,int digit) {
-        if (n == 0){
+
+    public static int countDigit(int n, int digit) {
+        if (n == 0) {
             return 0;
-    }
-        int lastChar=n%10;
+        }
+        int lastChar = n % 10;
 
-         if(lastChar==digit) {
-        return 1 + countDigit(n / 10, digit);
-    }
-            else
-            return countDigit(n/10,digit);
+        if (lastChar == digit) {
+            return 1 + countDigit(n / 10, digit);
+        } else
+            return countDigit(n / 10, digit);
 
 
     }
+
+    public static int handShake(int n) {
+        if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        }
+        else
+            return n-1+handShake(n-1);
+    }
+
 }
+
+
+
+
