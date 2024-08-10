ea0bbdc7278e39a5e2abc9b9f608790b1671cb2b
Ahmad07o <aj120262@gmail.com>
2024-08-10 17:41:35 +0500

    factorail

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(factorial(7));
+    }
+    public static int factorial(int num)
+    {
+        if(num==1)
+            return 1;
+        else
+            return num*factorial(num-1);
+    }
+}
